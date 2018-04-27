17b396fbc6fccbe9ad7574d8b92c0952811d4eed
qigao1 <38788608+qigao1@users.noreply.github.com>
2018-04-27 16:18:46 -0400

    Create __init__.py

---
+++ kmeansmethods/kmeanspython/__init__.py
@@ -0,0 +1,4 @@
+from kmeanspython.kmeansinit import kmeansinit
+from kmeanspython.kmeansloop import kmeansloop
+from kmeanspython.kmeansinitpp import kmeansinitpp
+from kmeanspython.kmeansinitvv import kmeansinitvv
