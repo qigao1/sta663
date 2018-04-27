78f25fd1c40fdd8c09a73cf6a4b519a01904c020
qigao1 <38788608+qigao1@users.noreply.github.com>
2018-04-27 16:21:45 -0400

    Create kmeansloop.py

---
+++ kmeansmethods/kmeanspython/kmeansloop.py
@@ -0,0 +1,26 @@
+import numpy as np
+from operator import itemgetter
+
+def kmeansloop(X, init, maxiter = 100, tol = 1e-4):
+    oldcenter = init
+    l, n = X.shape
+    k = len(init)
+    count = 0
+    while count < maxiter:
+        count += 1
+        clulist = np.zeros(l)
+        dislist = np.zeros(l)
+        for i in range(l):
+            dist = np.apply_along_axis(np.linalg.norm, 1, oldcenter - X[i].reshape((1,-1)))
+            clulist[i], dislist[i] = min(enumerate(dist), key=itemgetter(1)) 
+        cluscenter = np.unique(clulist)
+        newcenter = np.where(np.repeat(np.array([i in cluscenter for i in range(k)]),n).reshape((k,n)), 
+                             np.array(list(map(lambda z: np.where(np.sum(clulist == z) > 0, 
+                                                                  np.mean(X[clulist == z],axis = 0), 
+                                                                  np.zeros(n)), range(k)))),
+                             oldcenter)
+        if np.allclose(oldcenter, newcenter, rtol=0, atol=tol):
+            break
+        oldcenter = newcenter
+    cost = np.sum(np.square(dislist))
+    return newcenter, clulist, count, cost
