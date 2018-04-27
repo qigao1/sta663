52eb3aeb86484017c481315efb3405d4f009eb34
qigao1 <38788608+qigao1@users.noreply.github.com>
2018-04-27 16:20:53 -0400

    Create kmeansinitpp.py

---
+++ kmeansmethods/kmeanspython/kmeansinitpp.py
@@ -0,0 +1,15 @@
+import numpy as np
+import random
+
+def kmeansinitpp(X, k):
+    n = X.shape[0]
+    C = X[random.randint(0, n-1)].reshape((1,-1))
+    while len(C) < k:
+        dislist = []
+        for i in range(len(X)):
+            dist = np.apply_along_axis(np.linalg.norm, 1, C - X[i].reshape((1,-1)))
+            dislist.append(np.square(np.min(dist)))
+        phi = np.sum(dislist)
+        prob = dislist / phi
+        C = np.vstack((C, X[np.random.choice(n, 1, p=prob)]))
+    return C
