17aa30b2aa5e77fb51b2bb46d4433e78e3e11b14
Yang1818 <38798369+Yang1818@users.noreply.github.com>
2018-04-28 00:33:49 -0400

    Update kmeansinit.py

--- kmeansmethods/kmeanspython/kmeansinit.py
+++ kmeansmethods/kmeanspython/kmeansinit.py
@@ -2,6 +2,9 @@ import random as random
 import numpy as np
 
 def kmeansinit(X, k):
+    """
+    Randomly generating initial centers
+    """
     l = len(X)
     lis = random.sample(list(np.arange(l)),k)
     return X[lis]
