9173c9eb511e936fe32e3391ec948f073d01657a
qigao1 <38788608+qigao1@users.noreply.github.com>
2018-04-30 00:36:09 -0400

    Update kmeansinitvv.py

--- kmeansmethods/kmeanspython/kmeansinitvv.py
+++ kmeansmethods/kmeanspython/kmeansinitvv.py
@@ -1,5 +1,7 @@
 import numpy as np
 import random
+from collections import Counter
+from operator import itemgetter
 from kmeanspython.kmeansinitpp import kmeansinitpp
 from kmeanspython.kmeansloop import kmeansloop
 
@@ -17,6 +19,11 @@ def kmeansinitvv(X, k, l):
             dist = np.apply_along_axis(np.linalg.norm, 1, C - X[j].reshape((1,-1)))
             dislist.append(np.square(np.min(dist)))
         phi = np.sum(dislist)
+    clulist = np.zeros(n)
+    for i in range(n):
+        dist = np.apply_along_axis(np.linalg.norm, 1, C - X[i].reshape((1,-1)))
+        clulist[i], dislist[i] = min(enumerate(dist), key=itemgetter(1))
+    weight = np.array(list(dict(Counter(clulist)).values()))
     init = kmeansinitpp(C, k)
-    out = kmeansloop(C, init, 10)
+    out = kmeansloop(C, init, 10, weight = weight)
     return out[0]
